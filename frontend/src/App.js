4df7fe1017f40a8e62bf4074be0c8943c2fbbda0
Felipe Duaibs <felipe.duaibs@hotmail.com>
2020-07-08 11:27:14 -0300

    Added material-ui framework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
+
 import './global.css';
 
 import Routes from './routes/routes';
@@ -8,9 +11,11 @@ import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes/>
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <Routes/>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#485345",
+    },
+    secondary: {
+      main: "#e1e087",
+    },
+  },
+});
+
+export default theme;
